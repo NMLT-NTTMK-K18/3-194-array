1499e7148d19fd89e6904815525337e40a380dec
NTGNguyen <23521049@gm.uit.edu.vn>
2023-10-04 14:33:15 +0700

    23521049

--- Bai003/Source.cpp
+++ Bai003/Source.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-
+void Nhap(float[], int&);
+void Xuat(float[], int);
+float TongAm(float[], int);
 int main()
 {
-
+	float b[100];
+	int k;
+	
+	Nhap(b, k);
+	cout << "\nMang ban dau : ";
+	Xuat(b, k);
+	
+	float kq = TongAm(b, k);
+	cout << "\nTong cac gia tri am : "<<setprecision(4)<<kq;
 	return 0;
+}
+void Nhap(float a[], int& n)
+{
+	cout << "Nhap n: ";
+	cin >> n;	
+	srand(time(NULL));
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = (rand() / (RAND_MAX / (100.0 - (-100.0)))) - 100.0;
+	}
+	
+}
+void Xuat(float a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << setw(8) << setprecision(2) << a[i];
+}
+float TongAm(float a[], int n)
+{
+	float s = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] < 0)
+			s += a[i];
+	}
+	return s;
 }
