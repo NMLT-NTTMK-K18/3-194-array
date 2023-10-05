8cfd4c3bfd3ea7b3a382a7930e387a4fbd5343fc
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-05 21:09:58 +0700

    Bai035. Completed

--- Bai035/Source.cpp
+++ Bai035/Source.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(int[], int&);
+int HangChuc(int);
+int TongGiaTri(int[], int);
+void Xuat(int);
+
 int main()
 {
+	int arr[500];
+	int n;
 
+	Nhap(arr, n);
+	cout << TongGiaTri(arr, n);
 	return 0;
+}
+
+void Nhap(int a[], int& nn)
+{
+	cout << "Nhap n: ";
+	cin >> nn;
+	for (int i = 0; i < nn; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+int HangChuc(int nn)
+{
+	int t = abs(nn);
+	return (t / 10) % 10;
+}
+
+int TongGiaTri(int a[], int nn)
+{
+	int s = 0;
+	for (int i = 0; i < nn; i++)
+	{
+		if (HangChuc(a[i]) == 5)
+			s += a[i];
+	}
+	return s;
 }
