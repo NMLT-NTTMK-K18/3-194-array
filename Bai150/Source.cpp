f78267e98580c997f526a69387a8be6bb8a49657
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-06 00:08:46 +0700

    Bai150. Completed

--- Bai150/Source.cpp
+++ Bai150/Source.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void Nhap(float[], int&);
+void Xuat(float[], int);
+void MotVeDau(float[], int);
 
 int main()
 {
+	float arr[500];
+	int n;
 
+	Nhap(arr, n);
+	MotVeDau(arr, n);
+	Xuat(arr, n);
 	return 0;
+}
+
+void Nhap(float a[], int& nn)
+{
+	cout << "Nhap n: ";
+	cin >> nn;
+	for (int i = 0; i < nn; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void Xuat(float a[], int nn)
+{
+	for (int i = 0; i < nn; i++)
+	{
+		cout << a[i] << setw(10);
+	}
+}
+
+void MotVeDau(float a[], int nn)
+{
+	int vt = 0;
+	for (int i = 0; i < nn; i++)
+	{
+		if (a[i] == 1)
+		{
+			float temp = a[i];
+			a[i] = a[vt];
+			a[vt] = temp;
+			vt++;
+		}
+	}
 }
