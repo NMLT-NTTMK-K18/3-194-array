90ea9e7388e9d9ba49d4eba4082443b9ab603947
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-05 22:51:30 +0700

    Bai 090. Completed

--- Bai085/Source.cpp
+++ Bai085/Source.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(int[], int&);
+int ChanCuoi(int[], int);
+
 int main()
 {
+	int arr[500];
+	int n;
 
+	Nhap(arr, n);
+	cout << ChanCuoi(arr, n);
 	return 0;
+}
+
+void Nhap(int a[], int& nn)
+{
+	cout << "Nhap n: ";
+	cin >> nn;
+	for (int i = 0; i < nn; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+int ChanCuoi(int a[], int nn)
+{
+	for (int i = nn - 1; i >= 0; i--)
+	{
+		if (a[i] % 2 == 0)
+			return a[i];
+	}
+	return -1;
 }
--- Bai090/Source.cpp
+++ Bai090/Source.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(int[], int&);
+bool ktHoanThien(int);
+int HoanThienDau(int[], int);
+
 int main()
 {
+	int arr[500];
+	int n;
 
+	Nhap(arr, n);
+	cout << HoanThienDau(arr, n);
 	return 0;
+}
+
+void Nhap(int a[], int& nn)
+{
+	cout << "Nhap n: ";
+	cin >> nn;
+	for (int i = 0; i < nn; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+bool ktHoanThien(int k)
+{
+	int s = 0;
+	for (int i = 1; i < k; i++)
+	{
+		if (k % i == 0)
+			s += i;
+	}
+	if (s == k)
+		return 1;
+	return 0;
+}
+
+int HoanThienDau(int a[], int nn)
+{
+	for (int i = 0; i < nn; i++)
+	{
+		if (ktHoanThien(a[i]))
+			return a[i];
+	}
+	return -1;
 }
