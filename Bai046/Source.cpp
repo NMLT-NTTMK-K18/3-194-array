9d688afefb619b662773b31ab9fbac337bd850da
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-10-06 00:28:41 +0700

    💥 Bai046

--- Bai046/Source.cpp
+++ Bai046/Source.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(int[], int &);
+
+bool ktNguyenTo(int);
+int DemNguyenTo(int[], int);
+int TongNguyenTo(int[], int);
+float TrungBinhCong(int[], int);
+
 int main()
 {
+	int array[100];
+	int n;
 
+	Nhap(array, n);
+	cout << "Trung binh cong cac so nguyen to: " << TrungBinhCong(array, n) << endl;
 	return 0;
+}
+
+void Nhap(float a[], int &n)
+{
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 0; i <= n - 1; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+bool ktNguyenTo(int k)
+{
+	int dem = 0;
+	for (int i = 1; i <= k; i++)
+		if (k % i == 0)
+			dem++;
+	if (dem == 2)
+		return true;
+	return false;
+}
+
+int DemNguyenTo(int a[], int n)
+{
+	int dem = 0;
+	for (int i = 0; i < n; i++)
+		if (ktNguyenTo(a[i]))
+			dem++;
+	return dem;
+}
+
+int TongNguyenTo(int a[], int n)
+{
+	int s = 0;
+	for (int i = 0; i < n; i++)
+		if (ktNguyenTo(a[i]))
+			s = s + a[i];
+	return s;
+}
+
+float TrungBinhCong(int a[], int n)
+{
+	int s = TongNguyenTo(a, n);
+	int dem = DemNguyenTo(a, n);
+	if (dem == 0)
+		return 0;
+	return (float)s / dem;
 }
