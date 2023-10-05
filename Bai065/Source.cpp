9ba97501c56097253de867493683a2adfa4b52d2
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-10-06 00:42:05 +0700

    💥 Bai065

--- Bai065/Source.cpp
+++ Bai065/Source.cpp
@@ -1,41 +1,40 @@
 #include <iostream>
 using namespace std;
 
-void Nhap(float[], int&);
+void Nhap(float[], int &);
 int DemPhanBiet(float[], int);
 
 int main()
 {
-	float arr[500];
+	float array[100];
 	int n;
 
-	Nhap(arr, n);
-	cout << DemPhanBiet(arr, n);
+	Nhap(array, n);
+	cout << "Dem phan biet: " << DemPhanBiet(array, n);
+
 	return 0;
 }
 
-void Nhap(float a[], int& nn)
+void Nhap(float a[], int &n)
 {
 	cout << "Nhap n: ";
-	cin >> nn;
-	for (int i = 0; i < nn; i++)
+	cin >> n;
+	for (int i = 0; i <= n - 1; i++)
 	{
 		cout << "Nhap a[" << i << "]: ";
 		cin >> a[i];
 	}
 }
 
-int DemPhanBiet(float a[], int nn)
+int DemPhanBiet(float a[], int n)
 {
 	int dem = 0;
-	for (int i = 0; i < nn; i++)
+	for (int i = 0; i < n; i++)
 	{
 		bool flag = true;
 		for (int j = 0; j < i; j++)
-		{
 			if (a[j] == a[i])
 				flag = false;
-		}
 		if (flag == true)
 			dem++;
 	}
