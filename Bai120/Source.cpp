becfdc0d89c7a4bad6b2318f5f461e9d110e0e84
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-05 23:31:50 +0700

    Bai125. Completed

--- Bai120/Source.cpp
+++ Bai120/Source.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(int[], int&);
+bool ktNguyenTo(int);
+int ktTonTai(int[], int);
+
 int main()
 {
+	int arr[500];
+	int n;
 
+	Nhap(arr, n);
+	cout << ktTonTai(arr, n);
 	return 0;
+}
+
+void Nhap(int a[], int& nn)
+{
+	cout << "Nhap n: ";
+	cin >> nn;
+	for (int i = 0; i < nn; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+bool ktNguyenTo(int k)
+{
+	int dem = 0;
+	for (int i=1; i<=k; i++)
+	{
+		if (k % i == 0)
+			dem++;
+	}
+	if(dem==2)
+		return 1;
+	return 0;
+}
+
+int ktTonTai(int a[], int nn)
+{
+	int flag = 0;
+	for (int i = 0; i < nn; i++)
+	{
+		if (ktNguyenTo(a[i]))
+			flag = 1;
+	}
+	return flag;
 }
--- Bai125/Source.cpp
+++ Bai125/Source.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(float[], int&);
+int ktTang(float[], int);
+
 int main()
 {
+	float arr[500];
+	int n;
 
+	Nhap(arr, n);
+	cout << ktTang(arr, n);
 	return 0;
+}
+
+void Nhap(float a[], int& nn)
+{
+	cout << "Nhap n: ";
+	cin >> nn;
+	for (int i = 0; i < nn; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+int ktTang(float a[], int nn)
+{
+	int flag = 1;
+	for (int i = 0; i < nn - 1; i++)
+			if (a[i] > a[i + 1])
+		flag = 0;
+	return flag;
 }
