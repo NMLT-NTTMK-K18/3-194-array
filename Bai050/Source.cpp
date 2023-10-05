7c7054f6f3c1ccd8e537be75c23d9b9b8d6f2d94
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-05 21:45:15 +0700

    Bai050. Completed

--- Bai050/Source.cpp
+++ Bai050/Source.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(int[], int&);
+int DemChan(int[], int);
+
 int main()
 {
+	int arr[500];
+	int n;
 
+	Nhap(arr, n);
+	cout << DemChan(arr, n);
 	return 0;
+}
+
+void Nhap(int a[], int& nn)
+{
+	cout << "Nhap n: ";
+	cin >> nn;
+	for (int i = 0; i < nn; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+int DemChan(int a[], int nn)
+{
+	int dem = 0;
+	for (int i = 0; i < nn; i++)
+	{
+		if (a[i] % 2 == 0)
+			dem++;
+	}
+	return dem;
 }
