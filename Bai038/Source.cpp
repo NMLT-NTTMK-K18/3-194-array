729271ba2692abc1b8fb4f08874f0a7128b46d3b
NTGNguyen <23521049@gm.uit.edu.vn>
2023-10-05 20:35:52 +0700

    Bai038.Done

--- Bai038/Source.cpp
+++ Bai038/Source.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void NhapMang(int[], int);
+int ChuSoDau(int);
+int TongGiaTri(int[], int);
 
 int main()
 {
-
+	int a[1000];
+	int n; cin >> n;
+	NhapMang(a, n);
+	cout << TongGiaTri(a, n);
 	return 0;
+}
+
+void NhapMang(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+}
+
+int ChuSoDau(int a[], int n)
+{
+	int t = abs(n);
+	while (t >= 10)
+		t /= 10;
+	return t;
+}
+
+int TongGiaTri(int a[], int n)
+{
+	int s = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (ChuSoDau(a[i]) & 1)
+			s += a[i];
+	}
+	return s;
 }
