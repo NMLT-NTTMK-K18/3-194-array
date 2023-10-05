0b47b311b6e048d4313b72fa6da782318032e9c0
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-05 21:01:48 +0700

    Bai030. Completed

--- Bai030/Source.cpp
+++ Bai030/Source.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void Nhap(float[], int&);
+void LietKe(float[], int);
 
 int main()
 {
+	float arr[500];
+	int n;
 
+	Nhap(arr, n);
+	LietKe(arr, n);
 	return 0;
+}
+
+void Nhap(float a[], int& nn)
+{
+	cout << "Nhap n: ";
+	cin >> nn;
+	for (int i = 0; i < nn; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+void LietKe(float a[], int nn)
+{
+	for (int i = 0; i < nn; i++)
+	{
+		for (int j = 0; j < nn; j++)
+		{
+			if (i != j)
+			{
+				cout << "(" << a[i] << ",";
+				cout << a[j] << ")";
+				cout << ", ";
+			}
+		}
+		cout << endl;
+	}
 }
