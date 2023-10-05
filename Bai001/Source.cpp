f9f6ce1e56f7ad956236bccfaa6f2569b99150da
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-10-05 14:12:35 +0700

    💥 Bai001

--- Bai001/Source.cpp
+++ Bai001/Source.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+void Nhap(int[], int &);
+void Xuat(int[], int);
 
 int main()
 {
+	int array[100], n;
+
+	Nhap(array, n);
+	Xuat(array, n);
 
 	return 0;
+}
+
+void Nhap(int a[], int &n)
+{
+	cout << "Nhap n: ";
+	cin >> n;
+	srand(time(NULL));
+	for (int i = 0; i <= n - 1; i++)
+		a[i] = rand() % (200 + 1) - 100;
+}
+
+void Xuat(int a[], int n)
+{
+	cout << n << endl;
+	for (int i = 0; i <= n - 1; i++)
+		cout << setw(10) << a[i];
 }
