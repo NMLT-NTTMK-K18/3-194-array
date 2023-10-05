95776a223b59b7f8a7803d9dcf99dc70a31d56da
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-05 22:11:44 +0700

    Bai075. Completed

--- Bai075/Source.cpp
+++ Bai075/Source.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(float[], int&, float&);
+float XaNhat(float[], int, float);
+
 int main()
 {
+	float arr[500];
+	int n;
+	float x;
 
+	Nhap(arr, n, x);
+	cout << XaNhat(arr, n, x);
 	return 0;
+}
+
+void Nhap(float a[], int& nn, float& xx)
+{
+	cout << "Nhap n: ";
+	cin >> nn;
+	for (int i = 0; i < nn; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+	cout << "Nhap x: ";
+	cin >> xx;
+}
+
+float XaNhat(float a[], int nn, float xx)
+{
+	float lc = a[0];
+	for (int i = 0; i < nn; i++)
+	{
+		if (abs(a[i] - xx) > abs(lc - xx))
+			lc = a[i];
+	}
+	return lc;
 }
