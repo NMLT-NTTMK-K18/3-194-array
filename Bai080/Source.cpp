b6eba2913402076cfdb5695384724b24b187e54d
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-05 22:41:05 +0700

    Bai080. Completed

--- Bai080/Source.cpp
+++ Bai080/Source.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
+void Nhap(float[], int&);
+float AmDau(float[], int);
+
 int main()
 {
+	float arr[500];
+	int n;
 
+	Nhap(arr, n);
+	cout << AmDau(arr, n);
+	return 0;
+}
+
+void Nhap(float a[], int& nn)
+{
+	cout << "Nhap n: ";
+	cin >> nn;
+	for (int i = 0; i < nn; i++)
+	{
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+}
+
+float AmDau(float a[], int nn)
+{
+	for (int i = 0; i < nn; i++)
+	{
+		if (a[i] < 0)
+			return a[i];
+	}
 	return 0;
 }
