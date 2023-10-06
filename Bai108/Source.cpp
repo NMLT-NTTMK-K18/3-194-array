2b6b428d6cf91416c830aef4a08fbaf72f2785f0
NTGNguyen <23521049@gm.uit.edu.vn>
2023-10-06 15:30:11 +0700

    Bai108.Done

--- Bai108/Source.cpp
+++ Bai108/Source.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 using namespace std;
 
+void NhapMang(int[], int);
+int ucln(int, int);
+int TimUCLN(int[], int);
+
 int main()
 {
-
+	int a[1000];
+	int n; cin >> n;
+	NhapMang(a, n);
+	cout << TimUCLN(a, n);
 	return 0;
+}
+
+void NhapMang(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+}
+
+int ucln(int a, int b)
+{
+	a = abs(a);
+	b = abs(b);
+	while (a * b != 0)
+	{
+		if (a > b)
+			a = a - b;
+		else
+			b = b - a;
+	}
+	return a + b;
+}
+
+int TimUCLN(int a[], int n)
+{
+	int lc = a[0];
+	for (int i = 0; i < n; i++)
+		lc = ucln(lc, a[i]);
+	return lc;
 }
