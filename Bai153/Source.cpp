7548ebb3b3f866c04b8ee286c03a542be8e9987c
NTGNguyen <23521049@gm.uit.edu.vn>
2023-10-06 21:49:58 +0700

    Bai153.Done

--- Bai153/Source.cpp
+++ Bai153/Source.cpp
@@ -1,8 +1,63 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void NhapMang(int[], int);
+void ChanVeDau(int[], int);
+void LeVeCuoi(int[], int);
+void ChanDauLeCuoi(int[], int);
+void XuatMang(int[], int);
 
 int main()
 {
-
+	int a[1000];
+	int n; cin >> n;
+	NhapMang(a, n);
+	ChanDauLeCuoi(a, n);
+	XuatMang(a, n);
 	return 0;
+}
+
+void NhapMang(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+}
+
+void ChanVeDau(int a[], int n)
+{
+	int vt = 0;
+	for (int i=0;i<n;i++)
+		if (a[i] % 2 == 0 && a[i] != 0)
+		{
+			int temp = a[i];
+			a[i] = a[vt];
+			a[vt] = temp;
+			vt++;
+		}
+}
+
+void LeVeCuoi(int a[], int n)
+{
+	int vt = n - 1;
+	for (int i = n - 1; i >= 0; i--)
+		if (a[i] % 2 != 0)
+		{
+			int temp = a[i];
+			a[i] = a[vt];
+			a[vt] = temp;
+			vt--;
+		}
+}
+
+void ChanDauLeCuoi(int a[],int n)
+{
+	ChanVeDau(a, n);
+	LeVeCuoi(a, n);
+}
+
+void XuatMang(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << a[i] << setw(8);
 }
