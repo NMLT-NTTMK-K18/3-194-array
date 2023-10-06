976dc42e29206c9429c3061c95d68abc7f0de550
NTGNguyen <23521049@gm.uit.edu.vn>
2023-10-06 15:41:16 +0700

    Bai113.Done

--- Bai113/Source.cpp
+++ Bai113/Source.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 using namespace std;
 
+void NhapMang(int[], int);
+int ViTriLonNhat(int[], int);
+int TimChuSo(int[], int);
+
 int main()
 {
-
+	int a[1000];
+	int n; cin >> n;
+	NhapMang(a, n);
+	cout << TimChuSo(a, n);
 	return 0;
+}
+
+void NhapMang(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+}
+
+int ViTriLonNhat(int a[], int n)
+{
+	int lc = 0;
+	for (int i = 0; i < n; i++)
+		if (a[i] > a[lc])
+			lc = i;
+	return lc;
+}
+
+int TimChuSo(int a[], int n)
+{
+	int dem[10] = { 0 };
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] == 0)
+			dem[0]++;
+		int t = abs(a[i]);
+		while (t != 0)
+		{
+			int dv = t % 10;
+			dem[dv]++;
+			t /= 10;
+		}
+	}
+	return ViTriLonNhat(dem, 10);
 }
