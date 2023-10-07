0b0cd343114e7bfdd3df09fad1b37f67fd0a6777
MinhHuy1507 <144987616+MinhHuy1507@users.noreply.github.com>
2023-10-07 16:38:34 +0700

    049 Done

--- Bai049/Source.cpp
+++ Bai049/Source.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
+void Nhap(float[], int&);
+float Trungbinh(float[], int);
 
 int main()
 {
+	float arr[100];
+	int n;
 
+	Nhap(arr, n);
+	cout << "Khoang cach trung giua cac cap gia tri: " << Trungbinh(arr, n);
 	return 0;
+}
+
+void Nhap(float a[], int& n)
+{
+	cout << "Nhap so phan tu cua mang: ";
+	cin >> n;
+	for (int i = 0; i <= n - 1; i++)
+	{
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+	}
+}
+
+float Trungbinh(float a[], int n)
+{
+	float s = 0;
+	int dem = 0;
+	for (int i = 0; i <= n - 1; i++)
+		for (int j = 0; j <= n - 1; j++)
+			if (i != j)
+			{
+				s = s + abs(a[i] - a[j]);
+				dem++;
+			}
+	if (dem == 0)
+		return 0;
+	return s / dem;
 }
