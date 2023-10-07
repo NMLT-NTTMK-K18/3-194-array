97f50c34f4233ef0ee720e209fba7e4650d89dff
MinhHuy1507 <144987616+MinhHuy1507@users.noreply.github.com>
2023-10-07 16:41:04 +0700

    079 Done

--- Bai079/Source.cpp
+++ Bai079/Source.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 using namespace std;
+void Nhap(float[], int&);
+float DuongDau(float[], int);
 
 int main()
 {
+	float arr[100];
+	int n;
 
+	Nhap(arr, n);
+	cout << "Gia tri duong dau tien cua mang la: " << DuongDau(arr, n);
+	return 0;
+}
+
+void Nhap(float a[], int& n)
+{
+	cout << "Nhap so phan tu mang: ";
+	cin >> n;
+	for (int i = 0; i <= n - 1; i++)
+	{
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+	}
+}
+
+float DuongDau(float a[], int n)
+{
+	for (int i = 0; i <= n - 1; i++)
+		if (a[i] > 0)
+			return a[i];
 	return 0;
 }
