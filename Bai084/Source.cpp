e954dc4280d042de68a58397fb181acefbfb74ea
MinhHuy1507 <144987616+MinhHuy1507@users.noreply.github.com>
2023-10-07 16:41:26 +0700

    084 Done

--- Bai084/Source.cpp
+++ Bai084/Source.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+void Nhap(float[], int&);
+int Vitri(float[], int);
 
 int main()
 {
+	float arr[100];
+	int n;
 
+	Nhap(arr, n);
+	cout << "Vi tri co 2 gia tri lan can va bang tich 2 vi tri lan can la:  " << Vitri(arr, n);
 	return 0;
+}
+
+void Nhap(float a[], int& n)
+{
+	cout << "Nhap so phan tu mang: ";
+	cin >> n;
+	for (int i = 0; i <= n - 1; i++)
+	{
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+	}
+}
+
+int Vitri(float a[], int n)
+{
+	for (int i = 1; i <= n - 2; i++)
+		if (a[i] == a[i + 1] * a[i - 1])
+			return i;
+	return -1;
 }
