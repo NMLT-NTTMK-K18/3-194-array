cda7f72547fb10773f7bef3bb3c0c77d28252d16
MinhHuy1507 <144987616+MinhHuy1507@users.noreply.github.com>
2023-10-08 20:20:51 +0700

    104-139 Done

--- Bai129/Source.cpp
+++ Bai129/Source.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 using namespace std;
+void Nhap(int[], int&);
+int KtSong(int[], int);
 
 int main()
 {
+	int arr[100];
+	int n;
+
+	Nhap(arr, n);
+	cout << KtSong(arr, n);
 
 	return 0;
+}
+
+void Nhap(int a[], int& n)
+{
+	cout << "Nhap so phan tu mang: ";
+	cin >> n;
+	for (int i = 0; i <= n - 1; i++)
+	{
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+	}
+}
+
+int KtSong(int a[], int n)
+{
+	if (n <= 1)
+		return 0;
+	if (n == 2)
+	{
+		if (a[0] == a[1])
+			return 0;
+		else
+			return 1;
+	}
+	int flag = 1;
+	for (int i = 1; i <= n - 2; i++)
+		if ((a[i] - a[i - 1]) * (a[i] - a[i + 1]) <= 0)
+			flag = 0;
+	return flag;
 }
--- Bai134/Source.cpp
+++ Bai134/Source.cpp
@@ -1,8 +1,56 @@
 #include <iostream>
 using namespace std;
+void Nhap(int[], int&);
+bool KtNguyento(int);
+void Xaydung(int[], int, int[], int&);
 
 int main()
 {
+	int arr[100];
+	int n;
+	int brr[100];
+	int k;
 
+	Nhap(arr, n);
+	Xaydung(arr, n, brr, k);
 	return 0;
+}
+
+void Nhap(int a[], int& n)
+{
+	cout << "Nhap so phan tu mang: ";
+	cin >> n;
+	for (int i = 0; i <= n - 1; i++)
+	{
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+	}
+}
+
+bool KtNguyento(int k)
+{
+	int dem = 0;
+	int i = 1;
+	while (i <= k)
+	{
+		if (k % i == 0)
+			dem++;
+		i++;
+	}
+	if (dem == 2)
+		return true;
+	return false;
+}
+
+void Xaydung(int a[], int n, int b[], int& k)
+{
+	k = 0;
+	cout << "Mang b gom cac so nguyen to cua mang a la: \n";
+	for (int i = 0; i < n; i++)
+		if (KtNguyento(a[i]))
+		{
+			b[k] = a[i];
+			cout << "b[" << k << "] = " << b[k] << "\n";
+			k++;
+		}
 }
