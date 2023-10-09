6d6f8e9fb0084bec40b4d553624a596705b1f7e5
MinhHuy1507 <144987616+MinhHuy1507@users.noreply.github.com>
2023-10-09 22:50:33 +0700

    144-159 Done

--- Bai149/Source.cpp
+++ Bai149/Source.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 using namespace std;
+void Nhap(float[], int&);
+void NguyenHoa(float[], int);
 
 int main()
 {
+	float arr[100];
+	int n;
 
+	Nhap(arr, n);
+	NguyenHoa(arr, n);
 	return 0;
+}
+
+void Nhap(float a[], int& n)
+{
+	cout << "Nhap so phan tu mang: ";
+	cin >> n;
+	for (int i = 0; i <= n - 1; i++)
+	{
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+	}
+}
+
+void NguyenHoa(float a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > 0)
+			a[i] = int(a[i] + 0.5);
+		else
+			a[i] = int(a[i] - 0.5);
+	}
+	cout << "Sau nguyen hoa: \n";
+	for (int i = 0; i < n; i++)
+		cout << "a[" << i << "] = " << a[i] << endl;
 }
--- Bai154/Source.cpp
+++ Bai154/Source.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 using namespace std;
+void Nhap(float[], int&);
+void DaoMang(float[], int);
 
 int main()
 {
+	float arr[100];
+	int n;
 
+	Nhap(arr, n);
+	DaoMang(arr, n);
 	return 0;
+}
+
+void Nhap(float a[], int& n)
+{
+	cout << "Nhap so phan tu mang: ";
+	cin >> n;
+	for (int i = 0; i <= n - 1; i++)
+	{
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+	}
+}
+
+void DaoMang(float a[], int n)
+{
+	int d = 0;
+	int c = n - 1;
+	while (d < c)
+	{
+		float temp = a[d];
+		a[d] = a[c];
+		a[c] = temp;
+		d++;
+		c--;
+	}
+	cout << "Sau dao mang: \n";
+	for (int i = 0; i < n; i++)
+		cout << "a[" << i << "] = " << a[i] << endl;
 }
