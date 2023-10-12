6d23653e9bd91aaa0630414a7ec6e2903a36d2ff
khiemhoangtruongUIT <137704231+truonghoangkhiem@users.noreply.github.com>
2023-10-12 15:07:00 +0700

    fix error with wrong results

--- Bai147/Source.cpp
+++ Bai147/Source.cpp
@@ -7,49 +7,46 @@ void Tron(int[], int, int[], int, int[], int&);
 
 int main()
 {
-	int n;
-	int a[10000];
-	int b[10000];
-	int m;
-	int c[10000];
-	int p;
-	Nhap(a, n,b,m);
-	Tron(a, n,b,m,c,p);
+	int a[10000], b[10000], c[100000];
+	int n, m, p;
+	Nhap(a, n, b, m);
+	Tron(a, n, b, m, c, p);
 	return 0;
 }
 
-void Nhap(int a[], int& n,int b[],int&m)
+void Nhap(int a[], int& n, int b[], int& m)
 {
 	cout << "Nhap n: ";
 	cin >> n;
-	cout << "Nhap m:";
+	cout << "Nhap m: ";
 	cin >> m;
 	for (int i = 0; i < n; i++)
 	{
 		cout << "a[" << i << "] = ";
 		cin >> a[i];
 	}
-	for (int i = 0; i < m; i++)
+	for (int k = 0; k < m; k++)
 	{
-		cout << "b[" << i << "] = ";
-		cin >> b[i];
+		cout << "b[" << k << "] = ";
+		cin >> b[k];
 	}
 }
 
-void Tron(int a[], int n, int b[], int m,
-	int c[], int& p)
+void Tron(int a[], int n, int b[], int m, int c[], int& p)
 {
-	int i = 0;
-	int j = 0;
-	p = 0;
-	while (!(i >= n && j >= m))
 	{
-		if ((i < n && j<m && a[i]>b[j]) || (j >= m))
-			c[p] = a[i];
-		 else
-			 c[p] = b[j];
-	}
-	cout << "Mang la: ";
+		int i = n-1;
+		int j = m-1;
+		p = 0;
+		while (!(i<0 && j<0))
+		{
+			if ((i>=0 && j>=0 && a[i]>b[j]) || (j<0))
+				c[p++] = a[i--];
+			else
+				c[p++] = b[j--];
+		}
+    }
+	cout << "Mang moi la: ";
 	for (int i = 0; i < p; i++)
-		cout << c[i]<<setw(9);
+		cout << c[i] << setw(8);
 }
